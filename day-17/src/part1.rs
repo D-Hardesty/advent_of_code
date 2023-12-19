ba72cad5a31707748fb32ff15efa260e64284bd6
D-Hardesty <dhardesty.py@gmail.com>
2023-12-19 15:30:36 -0700

    day-17 graph set up.

--- day-17/src/part1.rs
+++ day-17/src/part1.rs
@@ -1,10 +1,26 @@
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashSet};
 use crate::custom_error::AocError;
 
 #[tracing::instrument]
 pub fn process(
     _input: &str,
 ) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+    let graph: Vec<Vec<i32>> = _input
+        .lines()
+        .map(|line| line
+            .chars()
+            .map(|ch| ch.to_digit(10).unwrap() as i32).collect()
+        ).collect();
+
+    println!("{:?}", graph);
+
+    let start = (0, 0);
+    let end = (graph[0].len() - 1, graph.len() - 1);
+
+    let result = 0;
+
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -13,9 +29,8 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
         let input = include_str!("../test_input1.txt");
-        assert_eq!("", process(input)?);
+        assert_eq!("102", process(input)?);
         Ok(())
     }
 }
