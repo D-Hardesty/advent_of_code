5f036fa9a45c45d6e9204406ff43127e2ae6d53c
D-Hardesty <dhardesty.py@gmail.com>
2024-01-02 20:10:57 -0700

    day 24 part 1 complete

--- day-24/src/part1.rs
+++ day-24/src/part1.rs
@@ -4,7 +4,118 @@ use crate::custom_error::AocError;
 pub fn process(
     _input: &str,
 ) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+    let hailstones: Vec<Vec<(f64, f64, f64)>> = _input
+        .lines()
+        .map(|line| {
+            line
+                .split('@')
+                .map(|s| {
+                    s.split(',')
+                        .map(|ch| ch.trim().parse::<f64>().unwrap())
+                        .collect::<Vec<f64>>()
+                })
+                .map(|v| {
+                    (
+                        v[0],
+                        v[1],
+                        v[2],
+                    )
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect();
+
+    // println!("{:?}", hailstones);
+
+    // create target bounds
+    let bounds: (f64, f64) = (200_000_000_000_000.0, 400_000_000_000_000.0);
+    // let bounds: (f64, f64) = (7.0, 27.0);
+
+
+    let mut result = 0;
+    for (index, stone_a) in hailstones.iter().enumerate() {
+        let second_point = (stone_a[0].0 - stone_a[1].0, stone_a[0].1 - stone_a[1].1);
+        // find slope for primary stone
+        let a_m = calculate_slope(stone_a[0].0, stone_a[0].1, second_point.0, second_point.1);
+        let a_b = calculate_intercept(stone_a[0].0, stone_a[0].1, a_m);
+        for stone_b in (index + 1)..hailstones.len() {
+            let second_point = (
+                hailstones[stone_b][0].0 - hailstones[stone_b][1].0,
+                hailstones[stone_b][0].1 - hailstones[stone_b][1].1
+            );
+            let b_m = calculate_slope(
+                hailstones[stone_b][0].0,
+                hailstones[stone_b][0].1,
+                second_point.0,
+                second_point.1,
+            );
+            let b_b = calculate_intercept(hailstones[stone_b][0].0, hailstones[stone_b][0].1, b_m);
+            // println!("a_m: {:?}, a_b: {:?}", a_m, a_b);
+            // println!("b_m: {:?}, b_b: {:?}", b_m, b_b);
+
+            if let Some(intersect_point) = line_intersect(a_m, a_b, b_m, b_b) {
+
+                // check a
+                if !check_if_already_passed(stone_a, intersect_point) {
+                    continue;
+                }
+                // check b
+                if !check_if_already_passed(&hailstones[stone_b], intersect_point) {
+                    continue;
+                }
+
+                if intersect_point.0 >= bounds.0
+                    && intersect_point.0 <= bounds.1
+                    && intersect_point.1 >= bounds.0
+                    && intersect_point.1 <= bounds.1
+                {
+                    // println!("stone_a: {:?}, slope: {:?}, stone_b: {:?}, slope: {:?}, intersection: {:?}", stone_a[0], a_m,
+                    //          hailstones[stone_b][0], b_m, intersect_point);
+                    // println!("Passed");
+                    result += 1;
+                }
+            }
+        }
+    }
+
+    Ok(result.to_string())
+}
+
+fn check_if_already_passed(origin: &Vec<(f64, f64, f64)>, intersect_point: (f64, f64)) -> bool {
+    let origin_x = (origin[0].0, origin[1].0);
+    let origin_y = (origin[0].1, origin[1].1);
+
+    if origin_x.1 < 0f64 && origin_x.0 < intersect_point.0 {
+        return false;
+    } else if origin_x.1 > 0f64 && origin_x.0 > intersect_point.0 {
+        return false;
+    }
+    if origin_y.1 < 0f64 && origin_y.0 < intersect_point.1 {
+        return false;
+    } else if origin_y.1 > 0f64 && origin_y.0 > intersect_point.1 {
+        return false;
+    }
+    true
+}
+
+
+fn calculate_intercept(x0: f64, y0: f64, slope: f64) -> f64 {
+    y0 - slope * x0
+}
+
+fn calculate_slope(x0: f64, y0: f64, x1: f64, y1: f64) -> f64 {
+    (y1 - y0) / (x1 - x0)
+}
+
+fn line_intersect(m1: f64, b1: f64, m2: f64, b2: f64) -> Option<(f64, f64)> {
+    if m1 == m2 {
+        // lines are parallel
+        None
+    } else {
+        let x = (b2 - b1) / (m1 - m2);
+        let y = m1 * x + b1;
+        Some((x, y))
+    }
 }
 
 #[cfg(test)]
@@ -13,9 +124,8 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
         let input = include_str!("../test_input1.txt");
-        assert_eq!("", process(input)?);
+        assert_eq!("2", process(input)?);
         Ok(())
     }
 }
--- day-24/src/part2.rs
+++ day-24/src/part2.rs
@@ -1,11 +1,109 @@
 use crate::custom_error::AocError;
+// use nalgebra::{Vector3, Point3, Vector2, Point2};
 
 #[tracing::instrument]
 pub fn process(
     _input: &str,
 ) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+    todo!("day 01 - part 1")
+    // let hailstones: Vec<Vec<(i64, i64, i64)>> = _input
+    //     .lines()
+    //     .map(|line| {
+    //         line
+    //             .split('@')
+    //             .map(|s| {
+    //                 s.split(',')
+    //                     .map(|ch| ch.trim().parse::<i64>().unwrap())
+    //                     .collect::<Vec<i64>>()
+    //             })
+    //             .map(|v| {
+    //                 (
+    //                     v[0],
+    //                     v[1],
+    //                     v[2],
+    //                 )
+    //             })
+    //             .collect::<Vec<_>>()
+    //     })
+    //     .collect();
+    //
+    // println!("{:?}", hailstones);
+    //
+    //
+    // let mut result = 0;
+    // for (index, stone) in hailstones.iter().enumerate() {
+    //     // assign start and vector to first stone
+    //     let primary_origin = Point2::new(stone[0].0, stone[0].1);
+    //     let primary_vec = Vector2::new(stone[1].0, stone[1].1);
+    //     println!("p_loc: {:?}, p_vec: {:?}", primary_origin, primary_vec);
+    //
+    //     for other in (index + 1)..hailstones.len() {
+    //         let secondary_origin = Point3::new(hailstones[other][0].0, hailstones[other][0].1, hailstones[other][0].2);
+    //         let secondary_vector = Vector3::new(hailstones[other][1].0, hailstones[other][0].1, hailstones[other][0]
+    //             .2);
+    //         println!("s_loc: {:?}, s_vec: {:?}", secondary_origin, secondary_vector);
+    //         let mut iter = 0;
+    //         loop {
+    //             let cur_p_loc = {
+    //                 (
+    //                     primary_location.0 + (primary_vector.0 * iter),
+    //                     primary_location.1 + (primary_vector.1 * iter),
+    //                     primary_location.2 + (primary_vector.2 * iter),
+    //                 )
+    //             };
+    //             let cur_s_loc = {
+    //                 (
+    //                     secondary_location.0 + (secondary_vector.0 * iter),
+    //                     secondary_location.1 + (secondary_vector.1 * iter),
+    //                     secondary_location.2 + (secondary_vector.2 * iter),
+    //                 )
+    //             };
+    //             if cur_p_loc.0 > lower_bound && cur_p_loc.0 < upper_bound {
+    //                 println!("cur_p: {:?}, cur_s: {:?}", cur_p_loc, cur_s_loc);
+    //                 if cur_p_loc == cur_s_loc {
+    //                     result += 1;
+    //                     break;
+    //                 }
+    //                 iter += 1;
+    //             } else {
+    //                 break;
+    //             }
+    //         }
+    //     }
+    // }
+    //
+    // Ok(result.to_string())
 }
+
+// fn check_intesect(a_origin: Point3<f64>,
+//                   a_direction: Vector3<f64>,
+//                   b_origin: Point3<f64>,
+//                   b_direction: Vector3<f64>,
+//                   start_point: Point3<f64>,
+//                   end_point: Point3<f64>,
+// ) -> bool {
+//     let v_a = a_direction;
+//     let v_b = b_direction;
+//
+//     let a_params = {
+//         let a_diff = start_point - a_origin;
+//         a_diff.component_div(&v_a)
+//     };
+//
+//     let b_params = {
+//         let b_diff = start_point - b_origin;
+//         b_diff.component_div(&v_b)
+//     };
+//
+//     // Check if A and B intersect within range
+//     let t_a_start = a_params.x.max(a_params.y.max(a_params.z));
+//     let t_a_end = a_params.x.min(a_params.y.min(a_params.z));
+//
+//     let t_b_start = b_params.x.max(b_params.y.max(b_params.z));
+//     let t_b_end = b_params.x.min(b_params.y.min(b_params.z));
+//
+//     t_a_start <= t_b_end && t_a_end >= t_b_start
+// };
 
 #[cfg(test)]
 mod tests {
